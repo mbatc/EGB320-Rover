d4598f333be992dc2f83d5b53778b05e5e8200eb
Mick Batchelor <mickbatch98@gmail.com>
2021-10-11 23:39:11 +1000

    import fusion

--- gyro.py
+++ gyro.py
@@ -1,20 +1,24 @@
-
+import sys
+sys.path.insert(0, './fusion')
 
 from math import *
 import time
 import copy
 import mpu6050
-from fusion.fusion import *
+from fusion import *
+
+def time_diff(start, end):
+  return end - start
 
 class Gyro:
   def __init__(self):
+    self.fusion = Fusion(time_diff)
     self.mpu    = mpu6050.mpu6050(0x68)
-    self.fusion = Fusion()
 
   def update(self):
     accel = self.mpu.get_accel_data()
     gryo  = self.mpu.get_gyro_data()
-    self.fusion.update_nomag((accel['x'], accel['y'], accel['z']), (gryo['x'], gryo['y'], gryo['z']))
+    self.fusion.update_nomag((accel['x'], accel['y'], accel['z']), (gryo['x'], gryo['y'], gryo['z']), time.perf_counter())
     self.ypr = [self.fusion.heading, self.fusion.pitch, self.fusion.roll]
 
 if __name__ == "__main__":
@@ -22,4 +26,5 @@ if __name__ == "__main__":
     while (1):
       gyro = Gyro()
       gyro.update()
-      print('ypr: {}'.format(gyro))
+      time.sleep(0.005)
+      print('ypr: {}'.format(gyro.ypr))
