60852b1bd4792b3544f583775f0522df4b3f4b75
Mick Batchelor <mickbatch98@gmail.com>
2021-10-13 17:11:54 +1000

    updated vision code

--- subsystems/vision/vision.py
+++ subsystems/vision/vision.py
@@ -3,8 +3,6 @@ import numpy as np
 import random as rng
 import time
 
-from ..interop import DetectedObject, ObjectType
-
 def Initialize():
     cap = cv2.VideoCapture(0)
     # Check if camera opened successfully
@@ -13,10 +11,11 @@ def Initialize():
         print("Error opening video stream or file")
     return cap
 
+
 def ObjectDetection(cap):
     ret, frame = cap.read()
-    objects = []
     if ret == True:
+            objects = []
             newtime = time.time()
             # Display the resulting frame
             # image = cv2.imread("image.png")
@@ -32,14 +31,17 @@ def ObjectDetection(cap):
             ORANGE_MIN1 = np.array([0,50,50])
             ORANGE_MAX1 = np.array([5, 255, 255])
 
-            BLUE_MIN = np.array([95,50,50])
-            BLUE_MAX = np.array([105,255,255])
+            BLUE_MIN = np.array([100,30,30])
+            BLUE_MAX = np.array([110,255,255])
 
             GREEN_MIN = np.array([35,30,30])
             GREEN_MAX = np.array([50,255,255])
 
-            YELLOW_MIN = np.array([55,30,30])
-            YELLOW_MAX = np.array([65,255,255])
+            YELLOW_MIN = np.array([25,100,100])
+            YELLOW_MAX = np.array([30,255,255])
+
+            BLACK_MIN = np.array([0,0,0])
+            BLACK_MAX = np.array([0,0,0])
 
             hsv_thresholded_orange1 = cv2.inRange(hsv_stream, ORANGE_MIN, ORANGE_MAX)
             hsv_thresholded_orange2 = cv2.inRange(hsv_stream, ORANGE_MIN1, ORANGE_MAX1)
@@ -50,6 +52,7 @@ def ObjectDetection(cap):
             hsv_thresholded_green = cv2.inRange(hsv_stream, GREEN_MIN, GREEN_MAX)
 
             hsv_thresholded_yellow = cv2.inRange(hsv_stream, YELLOW_MIN, YELLOW_MAX)
+            hsv_thresholded_black = cv2.inRange(hsv_stream, BLACK_MIN, BLACK_MAX)
 
 
 
@@ -65,17 +68,19 @@ def ObjectDetection(cap):
                 if Extent > 0.7:
                     cv2.rectangle(canny_edge_green,(x,y),(x+w,y+h),(150,255,0),2)
                     if(x>(cap.get(3)/2)):
-                         x = x - w
-                    cv2.putText(canny_edge_green,('Green, Height:'+str(h)),(x+w+10,y+h),0,0.3,(150,255,0))
+                         xtext = x
+                    else:
+                         xtext = x 
+                    cv2.putText(canny_edge_green,('Green, Height:'+str(h)),(xtext+w+10,y+h),0,0.3,(150,255,0))
                     D_DetermanentG = 8000
                     Distance = round((D_DetermanentG/h),2)
-                    cv2.putText(canny_edge_green,('Distance'+str(Distance)),(x+w+10,y-20),0,0.3,(150,255,0))
+                    cv2.putText(canny_edge_green,('Distance'+str(Distance)),(xtext+w+10,y-20),0,0.3,(150,255,0))
                     capwidth  = cap.get(3)
                     Bearing = (((x+(w/2))-(capwidth/2))/(7.25))
                     Bearing1 = round(Bearing,2)
-                    cv2.putText(canny_edge_green,('Bearing'+str(Bearing1)),(x+w+10,y-30),0,0.3,(150,255,0))
+                    cv2.putText(canny_edge_green,('Bearing'+str(Bearing1)),(xtext+w+10,y-30),0,0.3,(150,255,0))
 
-                    objects.append(DetectedObject(ObjectType.OBSTACLE,Bearing1,Distance,0)) #0 placeholder for orientation
+                    objects.append(DetectedObject("Green",Bearing1,Distance,0)) #0 placeholder for orientation
 
 
             #blue
@@ -90,7 +95,9 @@ def ObjectDetection(cap):
                 if Extent > 0.7:
                     cv2.rectangle(canny_edge_blue,(x,y),(x+w,y+h),(150,255,0),2)
                     if(x>(cap.get(3)/2)):
-                         x = x - w
+                         xtext = x
+                    else:
+                         xtext = x 
                     cv2.putText(canny_edge_blue,('Blue, Height:'+str(h)),(x+w+10,y+h),0,0.3,(150,0,0))
                     Oriantation = round(h/w,2)
                     if Oriantation >1:
@@ -100,16 +107,16 @@ def ObjectDetection(cap):
                         OriantationStr = "Straight"
                     else:
                         OriantationStr = str(Oriantation)
-                    cv2.putText(canny_edge_blue,('Oriantation'+OriantationStr),(x+w+10,y-10),0,0.3,(150,255,0))
+                    cv2.putText(canny_edge_blue,('Oriantation'+OriantationStr),(xtext+w+10,y-10),0,0.3,(150,255,0))
                     D_DetermanentB = 4200
                     Distance = round((D_DetermanentB/h),2)
-                    cv2.putText(canny_edge_blue,('Distance'+str(Distance)),(x+w+10,y-20),0,0.3,(150,255,0))
+                    cv2.putText(canny_edge_blue,('Distance'+str(Distance)),(xtext+w+10,y-20),0,0.3,(150,255,0))
                     capwidth  = cap.get(3)
                     Bearing = (((x+(w/2))-(capwidth/2))/(7.25))
                     Bearing1 = round(Bearing,2)
-                    cv2.putText(canny_edge_blue,('Bearing'+str(Bearing1)),(x+w+10,y-30),0,0.3,(150,255,0))
+                    cv2.putText(canny_edge_blue,('Bearing'+str(Bearing1)),(xtext+w+10,y-30),0,0.3,(150,255,0))
 
-                    objects.append(DetectedObject(ObjectType.ROCK,Bearing1,Distance,0)) #0 placeholder for orientation
+                    objects.append(DetectedObject("Blue",Bearing1,Distance,0)) #0 placeholder for orientation
 
 
             #Orange
@@ -124,18 +131,20 @@ def ObjectDetection(cap):
                  if Extent > 0.7:
                      cv2.rectangle(canny_edge_orange,(x,y),(x+w,y+h),(150,255,0),2)
                      if(x>(cap.get(3)/2)):
-                         x = x - w
+                         xtext = x
+                     else:
+                         xtext = x 
                      
-                     cv2.putText(canny_edge_orange,('Orange, Height:'+str(h)),(x+w+10,y+h),0,0.3,(150,255,0))
+                     cv2.putText(canny_edge_orange,('Orange, Height:'+str(h)),(xtext+w+10,y+h),0,0.3,(150,255,0))
                      D_DetermanentO = 2200
                      Distance = round((D_DetermanentO/h),2)
-                     cv2.putText(canny_edge_blue,('Distance'+str(Distance)),(x+w+10,y-20),0,0.3,(150,255,0))
+                     cv2.putText(canny_edge_blue,('Distance'+str(Distance)),(xtext+w+10,y-20),0,0.3,(150,255,0))
                      capwidth  = cap.get(3)
                      Bearing = ((x+(w/2))-(capwidth/2))/(7.25)
                      Bearing1 = round(Bearing,2)
-                     cv2.putText(canny_edge_orange,('Bearing'+str(Bearing1)),(x+w+10,y-30),0,0.3,(150,255,0))
+                     cv2.putText(canny_edge_orange,('Bearing'+str(Bearing1)),(xtext+w+10,y-30),0,0.3,(150,255,0))
 
-                     objects.append(DetectedObject(ObjectType.SAMPLE,Bearing1,Distance,0)) #0 placeholder for orientation
+                     objects.append(DetectedObject("Orange",Bearing1,Distance,0)) #0 placeholder for orientation
             
             #Yellow
             canny_edge_yellow = cv2.blur(hsv_thresholded_yellow, (10,10))
@@ -146,21 +155,47 @@ def ObjectDetection(cap):
                 
                  x,y,w,h = rect
                  Extent = (cv2.contourArea(c)/(w*h))
-                 if Extent > 0.7:
+                 if Extent > 0.3 and cv2.contourArea(c) > 10000:
                      cv2.rectangle(canny_edge_yellow,(x,y),(x+w,y+h),(150,255,0),2)
                      if(x>(cap.get(3)/2)):
-                         x = x - w
+                         xtext = x
+                     else:
+                         xtext = x 
                      
-                     cv2.putText(canny_edge_yellow,('Yellow, Height:'+str(h)),(x+w+10,y+h),0,0.3,(150,255,0))
-                     D_DetermanentO = 2200
-                     Distance = round((D_DetermanentO/h),2)
-                     cv2.putText(canny_edge_yellow,('Distance'+str(Distance)),(x+w+10,y-20),0,0.3,(150,255,0))
+                     cv2.putText(canny_edge_yellow,('Yellow, Height:'+str(h)),(xtext+w+10,y+h),0,0.3,(150,255,0))
+                     D_DetermanentY = 4000
+                     Distance = int(round((D_DetermanentY/h),2))
+                     cv2.putText(canny_edge_yellow,('Distance'+str(Distance)),(xtext+w+10,y-20),0,0.3,(150,255,0))
                      capwidth  = cap.get(3)
                      Bearing = ((x+(w/2))-(capwidth/2))/(7.25)
                      Bearing1 = round(Bearing,2)
                      cv2.putText(canny_edge_yellow,('Bearing'+str(Bearing1)),(x+w+10,y-30),0,0.3,(150,255,0))
 
-                     objects.append(DetectedObject(ObjectType.LANDER,Bearing1,Distance,0)) #0 placeholder for orientation
+                     objects.append(DetectedObject("Yellow",Bearing1,Distance,0)) #0 placeholder for orientation
+
+
+            #Black
+            canny_edge_black = cv2.blur(hsv_thresholded_black, (10,10))
+            _, contourso, hierarchy = cv2.findContours(canny_edge_black,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            for c in contourso:
+                 rect = cv2.boundingRect(c)
+                 if rect[2] < 80 or rect[3] < 80: continue
+                
+                 x,y,w,h = rect
+                 Extent = (cv2.contourArea(c)/(w*h))
+                 if Extent > 0.2 and cv2.contourArea(c) > 10000:
+                     cv2.rectangle(canny_edge_black,(x,y),(x+w,y+h),(150,255,0),2)
+                     if(x>(cap.get(3)/2)):
+                         xtext = x   
+                     else:
+                         xtext = x   
+                     #print(str(cv2.contourArea(c))
+                     cv2.putText(canny_edge_black,('Black, Height:'+str(h)),(xtext+w+10,y+h),0,0.3,(150,255,0))
+                     D_DetermanentO = 2200
+                     Distance = int(round((D_DetermanentO/h),2))
+                     cv2.putText(canny_edge_black,('Distance'+str(Distance)),(xtext+w+10,y-20),0,0.3,(150,255,0))
+            
+                     objects.append(DetectedObject("Wall",Bearing1,Distance,0)) #0 placeholder for orientation
 
 
             if ((time.time()-newtime)!=0):
@@ -169,11 +204,11 @@ def ObjectDetection(cap):
                 Freq = 0
 
 
-            img = canny_edge_blue + canny_edge_green + canny_edge_orange + canny_edge_yellow
-            #img  = canny_edge_yellow
+            img = canny_edge_blue + canny_edge_green + canny_edge_orange + canny_edge_yellow +canny_edge_black
+            
             cv2.putText(img,('Frequency: '+str(Freq)+"Hz"),(30,30),0,0.3,(150,255,0))
 
-            #cv2.imshow('Frame', hsv_stream)
+            cv2.imshow('Frame', hsv_stream)
             #cv2.imshow('Frame_Threshold_Orange', hsv_thresholded_orange)
             #cv2.imshow('Frame_Threshold_Blue', hsv_thresholded_blue)
             #cv2.imshow('Frame_Threshold_Green', hsv_thresholded_green)
@@ -181,5 +216,5 @@ def ObjectDetection(cap):
             cv2.imshow('Frame_Detections', img)
             # cv2.imshow('Frame_Threshold_edges_blue',  blue_edges)
             # cv2.imshow('Frame_Threshold_edges_orange',  orange_edges)
+            return objects
 
-    return objects
